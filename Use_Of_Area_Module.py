997dfc7a6b1f0f4ac273832c3cb913e74749aaa6
Seahawkkid000 <107086227+Seahawkkid000@users.noreply.github.com>
2023-09-04 19:03:06 -0500

    Add files via upload

---
+++ Use_Of_Area_Module.py
@@ -0,0 +1,5 @@
+import area
+area.area_circle(5)
+area.area_triangle(5,5)
+area.area_rectangle(5,5)
+area.area_square(5)
---
+++ area.py
@@ -0,0 +1,11 @@
+import math
+def area_circle(radius):
+    return(math.pi*radius*radius)
+def area_triangle(base,height):
+    return(base*height/2)
+
+def area_rectangle(length,width):
+    return(length*width)
+
+def area_square(side):
+    return(side**2)
